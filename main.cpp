0a9086965efa22103bbe96c9998013093eeb4c55
Laila Nuraini <145174255+lailanurainii@users.noreply.github.com>
2023-11-22 09:53:59 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//program Hello World
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//menampilkan tulisan "hello world" 
+	cout << "Hello WOrld..!\n";
+	cout << "Name  : Laila Nuraini\n";
+	cout << "NIM   : 23.11.5740\n" ;
+	cout << "Class : IF6\n";
+
+	system("pause");
+
+	return 0;
+}
